b3e3bebe91989f33d6badf5f2217087828de6ee7
moust1que <emiliensalpeteur@gmail.com>
2025-04-08 16:50:50 +0200

    Verbose

--- Assets/Features/BBehaviour/Runtime/BBehaviour.cs
+++ Assets/Features/BBehaviour/Runtime/BBehaviour.cs
@@ -1,7 +1,30 @@
 using UnityEngine;
 
 namespace BBehaviour.Runtime {
+    public enum VerboseType { Log, Warning, Error }
+
     public class BBehaviour : MonoBehaviour {
-        
+        #region Verbose
+            public bool isVerbose;
+            public bool logs;
+            public bool warnings;
+            public bool errors;
+
+            public void Verbose(string message, VerboseType type = VerboseType.Log) {
+                if(isVerbose) {
+                    switch(type) {
+                        case VerboseType.Log:
+                            if(logs) Debug.Log(message);
+                            break;
+                        case VerboseType.Warning:
+                            if(warnings) Debug.LogWarning(message);
+                            break;
+                        case VerboseType.Error:
+                            if(errors) Debug.LogError(message);
+                            break;
+                    }
+                }
+            }
+        #endregion
     }
 }
